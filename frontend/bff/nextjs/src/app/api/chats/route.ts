0872786f9d70f55b9b6c06c58fd5cc657d23f270
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-04-23 16:46:06 -0300

    feat: post message db mysql

--- frontend/bff/nextjs/src/app/api/chats/route.ts
+++ frontend/bff/nextjs/src/app/api/chats/route.ts
@@ -4,9 +4,29 @@ import { prisma } from "../../prisma/prisma";
 
 export async function POST (request: NextRequest) {
 
+    const body = await request.json();
     const chatCreated = await prisma.chat.create({
-        data: {},
+        data: {
+            messages: {
+                create: {
+                    content: body.message,
+                },
+            },
+        },
+        include:{
+            messages: true
+        }
     });
 
     return NextResponse.json(chatCreated);
+}
+
+export async function GET(request: NextRequest) {
+    const chats = await prisma.chat.findMany({
+        orderBy: {
+            created_at: "desc",
+        },
+    });
+
+    return NextResponse.json(chats);
 }
