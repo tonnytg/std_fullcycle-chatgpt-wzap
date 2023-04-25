85fc3af3762bbe04b813533c79a875d794a6e45e
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-04-24 23:36:47 -0300

    feat: add new route grpc

---
+++ frontend/bff/nextjs/src/grpc/client.ts
@@ -0,0 +1,18 @@
+import * as protoLoader from "@grpc/proto-loader";
+import * as grpc from "@grpc/grpc-js";
+import path from "path";
+import { ProtoGrpcType } from "./rpc/chat";
+import { hostname } from "os";
+
+const packageDefinition = protoLoader.loadSync(
+  path.resolve(process.cwd(), "proto", "chat.proto")
+);
+
+const proto = grpc.loadPackageDefinition(
+  packageDefinition
+) as unknown as ProtoGrpcType;
+
+export const chatClient = new proto.pb.ChatService(
+  "host.docker.internal:50051",
+  grpc.credentials.createInsecure()
+);
